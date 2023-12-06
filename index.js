fdca401dbeb9586d0efa7c79487527e248cdfad1
Tasnimul Haque <tasnimul.haque6@gmail.com>
2023-12-06 23:27:28 +0600

    cors origin added

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const options = [
+  cors({
+    origin: ['https://cubitose.netlify.app', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+];
+
+app.use(options);
 app.use(express.json());
 
 
@@ -38,17 +47,17 @@ async function run() {
     app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
-      res.send({token})
+      res.send({ token })
     })
 
     // insert portfolio data
-    app.post('/portfolio', async(req, res) => {
+    app.post('/portfolio', async (req, res) => {
       const newPortfolio = req.body;
       const result = await portfolio.insertOne(newPortfolio);
       res.send(result)
     })
     // Get Portfolio data
-    app.get('/portfolio', async(req, res) => {
+    app.get('/portfolio', async (req, res) => {
       const cursor = portfolio.find();
       const result = await cursor.toArray()
       res.send(result)
@@ -61,14 +70,14 @@ async function run() {
       res.send(result)
     })
 
-  // Insert Review data
-    app.post('/review', async(req, res) => {
+    // Insert Review data
+    app.post('/review', async (req, res) => {
       const newReview = req.body;
       const result = await review.insertOne(newReview);
       res.send(result)
     })
     // Get review data
-    app.get('/review', async(req, res) => {
+    app.get('/review', async (req, res) => {
       const cursor = review.find();
       const result = await cursor.toArray()
       res.send(result)
@@ -92,10 +101,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Cubitose is running!')
-    res.sendStatus(200)
+  res.send('Cubitose is running!')
+  res.sendStatus(200)
 })
 
 app.listen(port, () => {
-    console.log('Cubitose server is running on por', port)
+  console.log('Cubitose server is running on por', port)
 })
